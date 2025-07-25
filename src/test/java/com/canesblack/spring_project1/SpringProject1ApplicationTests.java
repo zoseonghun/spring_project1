a925ccea1f1936ba6ed53c01b1bf442d3f4ac16c
Seonghun <zoseonghun@gmail.com>
2025-07-25 21:29:07 +0900

    Initial commit

---
+++ src/test/java/com/canesblack/spring_project1/SpringProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canesblack.spring_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
