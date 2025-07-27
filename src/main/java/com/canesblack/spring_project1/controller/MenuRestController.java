41880fbd0a0f7e8ceff24f1805a1ac2ce4efc8d1
Seonghun <zoseonghun@gmail.com>
2025-07-27 21:57:38 +0900

    RestController 작성 완료

--- src/main/java/com/canesblack/spring_project1/controller/MenuRestController.java
+++ src/main/java/com/canesblack/spring_project1/controller/MenuRestController.java
@@ -5,10 +5,7 @@ import com.canesblack.spring_project1.service.MenuRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -43,5 +40,34 @@ public class MenuRestController {
         // 메뉴를 데이터베이스에 삽입
         menuRestService.boardInsert(menu);
         return ResponseEntity.ok("게시글 작성");
+    }
+
+    // 메뉴(한개의 게시판) 수정
+    @PutMapping("/menu/update/{idx}")
+    public void updateMenu(@RequestBody Menu menu,@PathVariable("idx") int idx) {
+        menu.setIdx(idx); // 특정 idx를 가진 게시글을 menu안의 title과 content를 가져와서 수정
+        menuRestService.boardUpdate(menu);
+    }
+
+    // 메뉴(한개의 게시판) 삭제
+    @DeleteMapping("/menu/delete/{idx}")
+    public void deleteMenu(@PathVariable("idx") int idx) {
+        menuRestService.boardDelete(idx);
+    }
+
+    // 특정메뉴(한개의 게시판 내용 조회) 조회
+    @GetMapping("/menu/{idx}")
+    public ResponseEntity<Menu> getMenu(@PathVariable("idx") int idx) {
+        Menu menu = menuRestService.boardContent(idx);
+        if(menu != null) {
+            return ResponseEntity.ok(menu);
+        } else
+            return ResponseEntity.notFound().build();
+    }
+
+    // 특정메뉴(게시판 조회수 증가) 조회수 증가
+    @PutMapping("/menu/count/{idx}")
+    public void incrementCount(@PathVariable("idx") int idx) {
+        menuRestService.boardCount(idx);
     }
 }
